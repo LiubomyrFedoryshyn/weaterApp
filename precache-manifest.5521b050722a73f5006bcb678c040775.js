117b93ad59c78164aa7d68bea1f78dd13008cb1d
robber <robber995@gmail.com>
2019-04-24 20:43:52 +0300

    Updates

---
+++ precache-manifest.5521b050722a73f5006bcb678c040775.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cd9ef854990da4ac9a720a8a4c252227",
+    "url": "/weaterApp/index.html"
+  },
+  {
+    "revision": "e6b9abb4583d19eacae9",
+    "url": "/weaterApp/static/css/main.967804d1.chunk.css"
+  },
+  {
+    "revision": "458a39bd49d85bb2560b",
+    "url": "/weaterApp/static/js/2.b0d4d72d.chunk.js"
+  },
+  {
+    "revision": "e6b9abb4583d19eacae9",
+    "url": "/weaterApp/static/js/main.a000f839.chunk.js"
+  },
+  {
+    "revision": "4b644c74f5f63144e43b",
+    "url": "/weaterApp/static/js/runtime~main.39539a63.js"
+  }
+]);
